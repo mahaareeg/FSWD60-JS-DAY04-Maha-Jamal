316449847ad16626e918f0ac26b866337eeee15f
JamalAhmed2019 <jamalusb@gmx.at>
2019-01-23 10:19:10 +0100

    substring merhod

--- basic.js
+++ basic.js
@@ -4,8 +4,15 @@ function age(bithyear){
   var age= cal - bithyear;
 
   document.write(age);
-  
+
 
 }
 
 age(1988);
+// Exercise two
+
+function subs(par1,par2){
+   var elstr= par1.substring(0, par2);
+   document.write("<br>" + elstr);
+}
+subs("I am a hero from World of Warcraft",12);
