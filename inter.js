e8a8e47c53a624fd5be8ba1f0741f2be90f177f8
JamalAhmed2019 <jamalusb@gmx.at>
2019-01-23 14:01:28 +0100

    editing HotelExercise

--- inter.js
+++ inter.js
@@ -1,6 +1,6 @@
-var night = prompt("Please enter night you went");
+var night = prompt("Please enter the hotel you want to book");
 
-function HotelConstructor(name,rooms,booked, Avnight,wantNight){
+function HotelConstructor(name,rooms,booked){
     this.name=name;
     this.rooms=rooms;
     this.booked=booked;
@@ -9,5 +9,34 @@ function HotelConstructor(name,rooms,booked, Avnight,wantNight){
         
     };
 }
-var hotel = new HotelConstructor("Areeg",20,10);
-console.log(hotel.checkAvailabilty());
+var random1=Math.floor(Math.random()*10)
+var random2=Math.floor(Math.random()*20)
+var random3=Math.floor(Math.random()*30)
+
+
+var areegHotel = new HotelConstructor("Areeg",10,random1);
+var memoHotel= new HotelConstructor("Memo",20,random2);
+var trendHotel= new HotelConstructor("Trend",30,random3);
+ if(areegHotel.name === 'Areeg'){
+
+ }
+if(areegHotel.checkAvailabilty() >= night){
+    document.getElementById('color').innerHTML="We have available rooms" + areegHotel;
+     document.getElementById("color").style.backgroundColor="#ff0076";
+}else {
+    document.write("sorry, no rooms available" + areegHotel)
+}
+
+
+
+if(memoHotel.checkAvailabilty() >= night){
+    document.write("i will resive")
+}else {
+    document.write("sorry, no rooms available")
+}
+if(trendHotel.checkAvailabilty() >= night){
+    document.write("i will resive")
+}else {
+    document.write("sorry, no rooms available")
+}
+
